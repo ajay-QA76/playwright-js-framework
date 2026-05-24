2859284f09c05e5e3171de2afa60e0bd42f7718f
ajay-QA76 <ajay.testing.work@gmail.com>
2026-05-24 17:30:58 +0530

    Update login.spec.js

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,1 +1,9 @@
+const { test, expect } = require('@playwright/test');
 
+test('Login Test', async ({ page }) => {
+
+    await page.goto('https://example.com');
+
+    console.log("Login Successful");
+
+});
